dfe7811b14c783959016dce9b139dfc7d6481453
Ashikur_Rahman <ashikur35-540@diu.edu.bd>
2022-11-08 18:44:55 +0600

    about and footer updated

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Header } from '../../Components/Header/Header'
+import { Milestone } from '../../Components/Milestone/Milestone'
 
 const Home = () => {
     return (
         <div className="min-h-screen">
             <Header />
+            <Milestone />
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    './src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'
+    './node_modules/tw-elements/dist/js/**/*.js'
   ],
   theme: {
 
