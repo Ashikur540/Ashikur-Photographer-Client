58fbf03e614fb1bdf243d97c51994e9435cc02a7
Ashikur_Rahman <ashikur35-540@diu.edu.bd>
2022-11-10 17:43:51 +0600

    blogs updating

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -9,15 +9,20 @@ const Blogs = () => {
 
             <div className="bg-base-100 px-6 py-3 text-left max-w-5xl border-4 rounded-lg shadow-lg mx-auto my-10">
                 <h1 className="text-neutral font-semibold py-4 text-xl"> Difference between SQL and NoSQL?</h1>
-                <p className="text-neutral">
-                    Cross-origin resource sharing (CORS) is a browser mechanism which enables controlled access to resources located outside of a given domain. It extends and adds flexibility to the same-origin policy (SOP). However, it also provides potential for cross-domain attacks, if a website's CORS policy is poorly configured and implemented. CORS is not a protection against cross-origin attacks such as cross-site request forgery (CSRF).
+                <p className="text-neutral list-decimal space-y-2">
+                    <li>SQL databases are relational, NoSQL databases are non-relational.</li>
+                    <li>SQL databases use structured query language and have a predefined schema. NoSQL databases have dynamic schemas for unstructured data.</li>
+                    <li>SQL databases are vertically scalable, while NoSQL databases are horizontally scalable.</li>
+                    <li>  SQL databases are table-based, while NoSQL databases are document, key-value, graph, or wide-column stores.</li>
+                    <li> SQL databases are better for multi-row transactions, while NoSQL is better for unstructured data like documents or JSON.</li>
                 </p>
             </div>
             <div className="bg-base-100 px-6 py-3 text-left max-w-5xl border-4 rounded-lg shadow-lg mx-auto my-10">
                 <h1 className="text-neutral font-semibold py-4 text-xl">What is JWT, and how does it work?</h1>
                 <p className="text-neutral">
-                    JWT is a token based technlogy which is used for authorization purpose. <br />
-                    Apart from that there are some other services we can use are <b>Heroku</b>,  <b>Deployd</b> ,<b>Kuzzle</b>, <b>Kumulos</b>
+                    JWT  or JSON Web Token, is an open standard used to share security information between two parties — a client and a server. Each JWT contains encoded JSON objects, including a set of claims. JWTs are signed using a cryptographic algorithm to ensure that the claims cannot be altered after the token is issued <br /><br />
+
+                    JWTs differ from other web tokens in that they contain a set of claims. Claims are used to transmit information between two parties. What these claims are depends on the use case at hand. For example, a claim may assert who issued the token, how long it is valid for, or what permissions the client has been granted.
 
                 </p>
             </div>
@@ -25,7 +30,7 @@ const Blogs = () => {
                 <h1 className="text-neutral font-semibold py-4 text-xl">What is the difference between javascript and NodeJS?</h1>
                 <p className="text-neutral">
                     <span className="font-semibold">Node.js: </span> is an open-source backend javascript runtime environment. It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node.js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive.<br />
-                    On the other hjand javascript is a programing language by basically interacts with the browser deals with objects, function, methods etc.There are many frame works of it. Node js is one of the frameworks tyhat provides runtime support.
+                    On the other hand <span className="font-semibold">Javascript </span> is a programing language by basically interacts with the browser deals with objects, function, methods etc.There are many frame works of it. Node js is one of the frameworks tyhat provides runtime support.
                 </p>
             </div>
             <div className="bg-base-100 px-6 py-3 text-left max-w-5xl border-4 rounded-lg shadow-lg mx-auto my-10">
